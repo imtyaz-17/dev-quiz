50054d437d5665e6adbf502de7535d8743205d3b
imtyaz-17 <imtyazit17017@gmail.com>
2024-05-05 22:40:08 +0600

    blog part - added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Main from './layout/Main';
 import Home from './components/Home/Home';
 import Quiz from './components/Quiz/Quiz';
+import Statistics from './components/Statistics/Statistics';
+import Blog from './components/Statistics/Blog/Blog';
 
 function App() {
   const router = createBrowserRouter([
@@ -23,6 +25,16 @@ function App() {
             return fetch(`https://openapi.programming-hero.com/api/quiz/${params.topicId}`)
           },
           element: <Quiz></Quiz>
+        },
+        {
+          path: "/statistics",
+          loader: () => fetch("https://openapi.programming-hero.com/api/quiz"),
+          element: <Statistics></Statistics>,
+        },
+        {
+          path: "/blog",
+          loader: () => fetch("https://openapi.programming-hero.com/api/quiz"),
+          element: <Blog></Blog>,
         },
       ]
     },
