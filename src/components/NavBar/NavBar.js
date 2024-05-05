01cdd5cefcc72d0bac9c8f783ca9a62609ddbaf5
imtyaz-17 <imtyazit17017@gmail.com>
2024-05-05 10:27:45 +0600

    navbar design

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <div>
-            <h2>Nav</h2>
-        </div>
+        <nav className='flex justify-between items-center py-2 px-12 bg-slate-500 text-white'>
+            <div>
+                <h2 className='text-3xl font-bold'>Dev-Quiz</h2>
+            </div>
+            <div className='flex space-x-8 text-xl font-semibold '>
+                <NavLink to='/home' className='hover:text-sky-200 active:text-sky-200 focus:outline-none focus:text-sky-300 '>Home</NavLink>
+                <NavLink className='hover:text-sky-200 active:text-sky-200 focus:outline-none focus:text-sky-300 ' to='/statistics'>Statistics</NavLink>
+                <NavLink className='hover:text-sky-200 active:text-sky-200 focus:outline-none focus:text-sky-300 ' to='/blog'>Blog</NavLink>
+                <NavLink className='hover:text-sky-200 active:text-sky-200 focus:outline-none focus:text-sky-300 ' to='/about'>About</NavLink>
+            </div>
+        </nav>
     );
 };
 
