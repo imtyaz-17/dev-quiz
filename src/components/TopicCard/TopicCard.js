141e71a837c8f0f2d92cf4721fe803977fac4546
imtyaz-17 <imtyazit17017@gmail.com>
2024-05-06 15:19:13 +0600

    project- done

--- src/components/TopicCard/TopicCard.js
+++ src/components/TopicCard/TopicCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ChevronDoubleRightIcon, EyeIcon } from '@heroicons/react/24/solid'
 const TopicCard = ({ topic }) => {
-    const { id, logo, name } = topic;
+    const { id, logo, name, total } = topic;
     const navigate = useNavigate();
     const handleNavigate = (id) => {
         navigate(`/topic/${id}`);
@@ -10,11 +10,17 @@ const TopicCard = ({ topic }) => {
     return (
         <div className="max-w-sm bg-white border border-sky-700 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-4">
             <img className="rounded-t-lg bg-slate-500" src={logo} alt="" />
-            <div className="p-5 flex justify-between">
-                <h5 className="mb-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-white ">{name}</h5>
-                <button onClick={() => handleNavigate(id)} className='inline-flex items-center p-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:outline-none dark:hover:bg-blue-700'>
-                    Start Quiz <ChevronDoubleRightIcon className="size-5"></ChevronDoubleRightIcon>
-                </button>
+            <div className="p-4">
+                <div className='flex justify-between items-center'>
+                    <h5 className=" text-2xl font-bold tracking-tight text-gray-900 dark:text-white ">{name}</h5>
+                    <p className='text-xl font-semibold text-gray-400 ml-'>{total} Questions</p>
+                </div>
+                <div>
+                    <button onClick={() => handleNavigate(id)} className='w-full justify-center inline-flex items-center mt-3 p-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:outline-none dark:hover:bg-blue-700'>
+                        Start Quiz <ChevronDoubleRightIcon className="size-5"></ChevronDoubleRightIcon>
+                    </button>
+                </div>
+
             </div>
         </div >
 
